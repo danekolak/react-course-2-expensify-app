6809d30dd5865c2acc1cdee790e02470798b93ce
Danijel Kolak <danijel.kolak123@hotmail.com>
2017-11-08 15:03:13 +0100

    Get firebase expenses on start

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,2 +1,6 @@
-import DotEnv from 'dotenv';
-DotEnv.config({ path: '.env.test' });
+require('dotenv').config({ path: '.env.test' });
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
